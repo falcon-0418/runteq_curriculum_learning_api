d76eb7a43cdbe7d9ea7fc1e15ea6b435ed486e5c
PIKO <shinsedaikakumei@gmail.com>
2023-10-14 21:36:26 +0900

    fix12

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -6,8 +6,7 @@ module Api
       include Api::ExceptionHandler
 
       private
-      # https://github.com/NoamB/sorcery/issues/724
-      # https://qiita.com/okaeri_ryoma/items/0d01469f2265e5d51af1
+  
       def form_authenticity_token; end
     end
   end
