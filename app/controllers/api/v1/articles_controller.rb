e1525d84220ec6541400d701baf0379e150077c2
PIKO <shinsedaikakumei@gmail.com>
2023-10-19 16:04:15 +0900

    fix23

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,39 +1,20 @@
+# frozen_string_literal: true
+
 module Api
   module V1
-    class User::ArticlesController < BaseController
-      before_action :set_articles, only: :index
-      before_action :set_article, only: %i[show update destroy]
+    class ArticlesController < BaseController
+      before_action :set_article, only: :show
 
       def index
-        json_string = ArticleSerializer.new(@articles).serialized_json
+        articles = Article.all
+        json_string = ArticleSerializer.new(articles).serialized_json
 
         render json: json_string
       end
 
-      def create
-        article = current_user.articles.new(article_params)
-
-        if article.save
-          json_string = ArticleSerializer.new(article).serialized_json
-          render json: json_string
-        else
-          render_400(nil, article.errors.full_messages)
-        end
-      end
-
-      def update
-        if @article.update(article_params)
-          json_string = ArticleSerializer.new(@article).serialized_json
-          render json: json_string
-        else
-          render_400(nil, article.errors.full_messages)
-        end
-      end
-
-      def destroy
-        @article.destroy!
-        set_articles
-        json_string = ArticleSerializer.new(@articles).serialized_json
+      def show
+        options = { include: [:user, :'user.name', :'user.email'] }
+        json_string = ArticleSerializer.new(@article, options).serialized_json
 
         render json: json_string
       end
@@ -41,15 +22,7 @@ module Api
       private
 
       def set_article
-        @article = current_user.articles.find(params[:id])
-      end
-
-      def set_articles
-        @articles = current_user.articles
-      end
-
-      def article_params
-        params.require(:article).permit(:title, :contents, :status)
+        @article = Article.find(params[:id])
       end
     end
   end
